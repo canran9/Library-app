24bb6b22e6b1c1df11c5ab7b07c23a2a43380988
canran9 <58532953+canran9@users.noreply.github.com>
2019-12-18 13:32:07 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BookComponent } from './book/book.component';
+import { BookService } from './service/book.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [BookService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
